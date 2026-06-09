eea02f208f4c76b9d0de87e48cd282d1b01b9246
denis-vyzulin <denis-vyzulin@mail.ru>
2026-06-09 11:21:55 +0300

    edit alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,53 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) return false;
+  for (int i = 2; i * i <= value; i++) {
+    if (value % i == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
+  uint64_t cnt = 0, i = 2;
+  while (true) {
+    if (checkPrime(i)) cnt++;
+    if (cnt == n) return i;
+    i++;
+  }
   return 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
+  while (true) {
+    if (checkPrime(++value)) return value;
+  }
   return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  uint64_t i = 2;
+  while (i < hbound) {
+    if (!checkPrime(i)) {
+      i++; continue;
+    }
+    sum += i; i++;
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t cnt = 0;
+  uint64_t prev = 0;
+  while (lbound < hbound) {
+    if (!checkPrime(lbound)) {
+      lbound++;
+      continue;
+    }
+    
+    if (prev != 0 && lbound == prev + 2) cnt++;
+    prev = lbound; lbound++;
+  }
+  return cnt;
 }
